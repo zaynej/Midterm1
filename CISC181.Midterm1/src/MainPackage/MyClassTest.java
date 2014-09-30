3cbcc70d0293fb88e3460401f312c1562413bf01
zaynej <zaynej@udel.edu>
2014-09-30 01:53:09 -0400

    JUnit

---
+++ CISC181.Midterm1/src/MainPackage/MyClassTest.java
@@ -0,0 +1,19 @@
+package MainPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyClassTest {
+
+	@Test
+	public void testReturnTrue() {
+		assertTrue("ReturnTrue returns true!", MyClass.ReturnTrue());
+	}
+
+	@Test
+	public void testAddTwoNumbers() {
+		assertEquals(10,MyClass.AddTwoNumbers(5,5));
+	}
+
+}
